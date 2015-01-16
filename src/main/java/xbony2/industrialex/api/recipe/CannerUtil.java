4aa12a7afb8e1b06654b5edfbc6a1962810f1faf
xbony2 <xbony22@gmail.com>
2015-01-16 17:59:37 -0500

    better support

---
+++ src/main/java/xbony2/industrialex/api/recipe/CannerUtil.java
@@ -0,0 +1,16 @@
+package xbony2.industrialex.api.recipe;
+
+import ic2.api.item.IC2Items;
+import net.minecraft.item.ItemStack;
+
+public class CannerUtil {
+	public final static IC2RecipeInput EMPTY_X1 = new IC2RecipeInput(IC2Items.getItem("tinCan"));
+	public final static IC2RecipeInput EMPTY_X3 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 3));
+	public final static IC2RecipeInput EMPTY_X4 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 4));
+	public final static IC2RecipeInput EMPTY_X8 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 8));
+	
+	public final static ItemStack FILLED_X1 = IC2Items.getItem("filledTinCan");
+	public final static ItemStack FILLED_X3 = new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 3);
+	public final static ItemStack FILLED_X4 = new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 4);
+	public final static ItemStack FILLED_X8 = new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 8);
+}
--- src/main/java/xbony2/industrialex/crossmod/natura/recipes/NaturaIC2Recipes.java
+++ src/main/java/xbony2/industrialex/crossmod/natura/recipes/NaturaIC2Recipes.java
@@ -4,55 +4,53 @@ import ic2.api.item.IC2Items;
 import ic2.api.recipe.Recipes;
 import mods.natura.common.NContent;
 import net.minecraft.item.ItemStack;
+import static xbony2.industrialex.api.recipe.CannerUtil.*;
 import xbony2.industrialex.api.recipe.IC2RecipeInput;
 
 public class NaturaIC2Recipes {
-	private final static ItemStack FILLED = IC2Items.getItem("filledTinCan");
 	
 	/**
 	 * Adds all the IC2 machine recipes for natura crossmod stuff.
 	 */
 	public static void addRecipes(){
-		IC2RecipeInput tincan = new IC2RecipeInput(IC2Items.getItem("tinCan"));
-		
 		IC2RecipeInput input1 = new IC2RecipeInput(new ItemStack(NContent.berryItem));
-		Recipes.cannerBottle.addRecipe(tincan, input1, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input1, FILLED_X1);
 		
 		IC2RecipeInput input2 = new IC2RecipeInput(new ItemStack(NContent.berryItem, 1, 1));
-		Recipes.cannerBottle.addRecipe(tincan, input2, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input2, FILLED_X1);
 		
 		IC2RecipeInput input3 = new IC2RecipeInput(new ItemStack(NContent.berryItem, 1, 2));
-		Recipes.cannerBottle.addRecipe(tincan, input3, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input3, FILLED_X1);
 		
 		IC2RecipeInput input4 = new IC2RecipeInput(new ItemStack(NContent.berryItem, 1, 3));
-		Recipes.cannerBottle.addRecipe(tincan, input4, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input4, FILLED_X1);
 		
 		IC2RecipeInput input5 = new IC2RecipeInput(new ItemStack(NContent.netherBerryItem));
-		Recipes.cannerBottle.addRecipe(tincan, input5, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input5, FILLED_X1);
 		
 		IC2RecipeInput input6 = new IC2RecipeInput(new ItemStack(NContent.netherBerryItem, 1, 1));
-		Recipes.cannerBottle.addRecipe(tincan, input6, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input6, FILLED_X1);
 		
 		IC2RecipeInput input7 = new IC2RecipeInput(new ItemStack(NContent.netherBerryItem, 1, 2));
-		Recipes.cannerBottle.addRecipe(tincan, input7, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input7, FILLED_X1);
 		
 		IC2RecipeInput input8 = new IC2RecipeInput(new ItemStack(NContent.netherBerryItem, 1, 3));
-		Recipes.cannerBottle.addRecipe(tincan, input8, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input8, FILLED_X1);
 		
 		IC2RecipeInput input9 = new IC2RecipeInput(new ItemStack(NContent.waterDrop));
-		Recipes.cannerBottle.addRecipe(tincan, input9, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X1, input9, FILLED_X1);
 		
 		IC2RecipeInput input10 = new IC2RecipeInput(new ItemStack(NContent.potashApple));
-		Recipes.cannerBottle.addRecipe(tincan, input10, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X4, input10, FILLED_X4);
 		
 		IC2RecipeInput input11 = new IC2RecipeInput(new ItemStack(NContent.seedFood));
-		Recipes.cannerBottle.addRecipe(tincan, input11, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X3, input11, FILLED_X3);
 		
 		IC2RecipeInput input12 = new IC2RecipeInput(new ItemStack(NContent.impMeat));
-		Recipes.cannerBottle.addRecipe(tincan, input12, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X3, input12, FILLED_X3);
 		
 		IC2RecipeInput input13 = new IC2RecipeInput(new ItemStack(NContent.impMeat, 1, 1));
-		Recipes.cannerBottle.addRecipe(tincan, input13, FILLED);
+		Recipes.cannerBottle.addRecipe(EMPTY_X8, input13, FILLED_X8);
 		
 	}
 }
